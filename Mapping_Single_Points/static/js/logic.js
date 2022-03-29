ee7939647b0a4aa6e3bde7f358f655482fcd2f7d
AditiOracle <95258178+AditiOracle@users.noreply.github.com>
2022-03-29 18:42:25 -0400

    third branch

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 //create the map object witha center and zoom level
-let map=L.map("mapid").setView([40.7,-94.5],4);
+let map=L.map("mapid").setView([34.0522, -118.2437],14);
 
 //This method is useful when we need to add multiple tile layers, or a background image of our map(s)
 
@@ -13,7 +13,15 @@ let map=L.map("mapid").setView([40.7,-94.5],4);
 // });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+L.circleMarker([34.0522, -118.2437], {
+    color:"black",
+    radius: 300,
+    fillColor:"#ffffa1"
+ }).addTo(map);
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
